6c0c0e7148fecbad97e1faad97fc46c0277f024a
Molly Wood <34974982+mollywood@users.noreply.github.com>
2018-05-01 13:18:38 -0500

    Update TipCalc.py

--- TipCalc.py
+++ TipCalc.py
@@ -1,5 +1,8 @@
+# ask the user to input the amounts
 total_amount = int(input("Enter the total amount: "))
 tip_percent = int(input("What percentage would you like to tip? "))
+# calculate the percentage by make tip_percent a decimal
 tip_per = tip_percent * .01
+#calculate the appropriate tip amount
 actual_tip = total_amount * tip_per
 print(f"The tip should be ${actual_tip}.")
